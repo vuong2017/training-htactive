063575a38dc50adcd46620e6c0690688a5d183ff
gldev6713 <gldev6713@gmail.com>
2018-11-19 21:47:18 +0700

    fix_webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const path = require('path');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 
 const plugins = [new FriendlyErrorsWebpackPlugin()];
 
@@ -15,7 +17,7 @@ if (!dev) {
   );
 }
 
-module.exports = {
+const browserConfig = {
   mode: dev ? 'development' : 'production',
   context: path.join(__dirname, 'src'),
   devtool: dev ? 'none' : 'source-map',
@@ -40,3 +42,28 @@ module.exports = {
   },
   plugins
 };
+
+const serverConfig = {
+  entry: './src/server/index.js',
+  target: 'node',
+  externals: [nodeExternals()],
+  output: {
+    path: __dirname,
+    filename: 'server.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __isBrowser__: "false"
+    })
+  ]
+}
+
+module.exports = [browserConfig, serverConfig]
+
+
