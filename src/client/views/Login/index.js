4c891e216ac6da5c3081fa4e7bc45b6d22690a08
Vuong Phan <vuongphan>
2018-12-26 13:38:38 +0700

    fe-ui-login

--- src/client/views/Login/index.js
+++ src/client/views/Login/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import Form from './Form'
+
 class Login extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Login Component</h1>
+      <div className="container">
+        <div className="row" id="pwd-container">
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+            <Form />
+          </div>
+          <div className="col-md-4"></div>
+        </div>
       </div>
     )
   }
